c3a11a99007f419b6f5f3b2aa43dac0ab582e383
ann-ngambi <ngambiann08@gmail.com>
2024-07-10 15:15:53 +0300

    second commit

--- index.php
+++ index.php
@@ -1,14 +1,33 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup page</title>
+</head>
 <body>
- 
-<?php
-echo "My first PHP script!";
-echo phpversion();
-?>
+    <form method="POST" action="login.php">
+        <label for="username">Username:</label><br>
+        <input type="text" id="username" placeholder="username"><br><br>
 
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" placeholder="password"><br><br>
+
+        <button type="submit">Login</button><br><br>
+    </form>
+<p>Signup instead</p><hr>
+    <form action="signup.php"  method="POST">
+    <label for="signup_username">Email:</label><br>
+    <input type="email" id="signup_username" placeholder="email"><br><br>
+
+        <label for="signup_password">Password:</label><br>
+        <input type="password" id="signup_password" placeholder="password"><br><br>
+
+        
+
+        <button type="submit">signup</button>
+    </form>
 </body>
 </html>
 
-http://login.php
 
--- signup.php
+++ signup.php
@@ -3,13 +3,6 @@ if (isset($_POST['signup'])) {
     $signup_username = $_POST['signup_username'];
     $signup_password = $_POST['signup_password'];
     $signup_email = $_POST['signup_email'];
-
-    // Insert the data into the database
-    $sql = "INSERT INTO users (username, password, email) VALUES ('$signup_username', md5('$signup_password'), '$signup_email')";
-    mysqli_query($conn, $sql);
-
-    // Sign up successful
-    echo "Sign up successful";
+    echo "Hello!" . $_POST['signup'];
 }
-
 ?>
